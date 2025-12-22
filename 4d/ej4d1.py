dcc8aaca44bc0e1ccd601cd4a27472afde601bd9
laiajudeztallon-blip <laiajudeztallon@gmail.com>
2025-12-22 15:20:53 +0100

    Update ej4d1.py

--- 4d/ej4d1.py
+++ 4d/ej4d1.py
@@ -36,7 +36,11 @@ Exemple:
 
 def sum():
     # Write here your code
-    pass
+    number_1 = int(input("Insert the first number: "))
+    number_2 = int(input("Insert the second number: "))
+    result = number_1 + number_2
+    print("Result: ", result)
+    return result
 
 
 # Si quieres probar tu código, descomenta las siguientes líneas y ejecuta el script
