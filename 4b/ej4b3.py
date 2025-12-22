61c49399b33eb3c4f77deb025528a1e0d26f8a20
laiajudeztallon-blip <laiajudeztallon@gmail.com>
2025-12-22 12:41:15 +0100

    Update ej4b3.py

--- 4b/ej4b3.py
+++ 4b/ej4b3.py
@@ -83,7 +83,19 @@ def create_list(length_list):
     """
 
     # Write here your code
-    pass
+    if length_list < 0:
+        raise ValueError("The number must be positive")
+
+    # Create a list in RAM
+    ram_list = []
+    for _ in range(length_list):
+        ram_list.append(random.randint(0, 100))
+
+    # Create a list in Heap by reusing RAM list
+    heap_list = copy.deepcopy(ram_list)
+
+    # Return both lists
+    return ram_list, heap_list
 
 
 # Si quieres probar tu código, descomenta las siguientes líneas y ejecuta el script
